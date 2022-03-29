df3b7b62b0f33d73623f37cafc9f611cc45c0bb6
Albin <albin@mullvad.net>
2022-03-29 14:53:02 +0200

    Propagate device state over service connection
    Adds device state communication between the app and service with a
    repository backed by a data source to add abstraction layers on the app
    side.
    
    Limitations:
    * Expiration is not updated correctly.
    * The login/creation flow has not been fully adapted to devices.

---
+++ android/app/src/main/kotlin/net/mullvad/mullvadvpn/model/DeviceState.kt
@@ -0,0 +1,29 @@
+package net.mullvad.mullvadvpn.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+sealed class DeviceState : Parcelable {
+    @Parcelize
+    object UnknownDeviceState : DeviceState()
+
+    @Parcelize
+    data class DeviceRegistered(val deviceConfig: DeviceConfig) : DeviceState()
+
+    @Parcelize
+    object DeviceNotRegistered : DeviceState()
+
+    fun isUnknown(): Boolean {
+        return this is UnknownDeviceState
+    }
+
+    fun token(): String? {
+        return (this as? DeviceRegistered)?.deviceConfig?.token
+    }
+
+    companion object {
+        fun fromDeviceConfig(deviceConfig: DeviceConfig?): DeviceState {
+            return deviceConfig?.let { DeviceRegistered(it) } ?: DeviceNotRegistered
+        }
+    }
+}
